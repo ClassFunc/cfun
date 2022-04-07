403cc4336013195313664f36e4db0941f9872c2f
LE VAN THANH <zero.lethanh@gmail.com>
2022-04-08 01:16:44 +0700

    add templates/https

---
+++ templates/https/onCall.js
@@ -0,0 +1,7 @@
+const functions = require('firebase-functions');
+
+module.exports = functions.
+    https.
+    onCall((data, context) => {
+
+    });
---
+++ templates/https/onRequest.js
@@ -0,0 +1,7 @@
+const functions = require('firebase-functions');
+
+module.exports = functions.
+    https.
+    onRequest((req, res) => {
+
+    });
